2909063e676f4b56603a889684515c5fc8ad5a09
Antoine Lafouasse <antoine.lafouasse@backmarket.com>
2023-03-29 10:58:08 +0200

    Setup sqladmin

--- sqladmin_poc/app.py
+++ sqladmin_poc/app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
 from .database import get_db
 from .database import Post
+from .database import get_url
+from sqlalchemy import create_engine
 from sqlalchemy import select
-
+from sqladmin import Admin, ModelView
 app = FastAPI()
 
+admin = Admin(app, create_engine(get_url()))
+
+
+class PostAdmin(ModelView, model=Post):
+    column_list = [Post.id, Post.content]
+
+
+admin.add_view(PostAdmin)
 
 @app.get("/")
 def root():
